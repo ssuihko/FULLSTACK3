d74ef00755e9f7d3934343408ceff8d4177f0da0
Sini Suihkonen <sahsuihkonen@gmail.com>
2023-12-14 22:41:52 +0200

    modify index

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 var morgan = require('morgan')
+app.use(express.static('dist'))
 
 app.use(express.json())
 app.use(cors())
